5b0724e16e22271af7edc8f2f851cd9655842aae
Ruben Lindes <ruben.lindes@iesnervion.es>
2022-11-18 13:36:42 +0100

    awa

---
+++ CRUD_Personas/CRUD_Personas_DAL/Utilidades/clsConexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_Personas_DAL.Utilidades
+{
+    public class clsConexion
+    {
+
+    }
+}
---
+++ CRUD_Personas/CRUD_Personas_Entidades/clsDepartamentos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_Personas_Entidades
+{
+    public class clsDepartamentos
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public clsDepartamentos()
+        {
+                
+        }
+        public clsDepartamentos(int id, string nombre)
+        {
+            Id = id;
+            Nombre = nombre;
+        }
+    }
+}
