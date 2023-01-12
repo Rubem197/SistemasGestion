873ad839b7d8c8587f6d5ea05689d79fe93e9124
Ruben Lindes <ruben.lindes@iesnervion.es>
2023-01-12 14:39:26 +0100

    awa

---
+++ HelloWorldJS/HelloWorldJS/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+//alert("Hola Ruben");
+
+window.onload = inicializaPagina;
+var contador = 0;
+
+function inicializaPagina() {
+    var miBoton = document.getElementById("btnSaludar").addEventListener("click", saludar, false);
+}
+
+function saludar() {
+    contador++;
+    document.getElementById("clickTotales").textContent = contador;
+}
